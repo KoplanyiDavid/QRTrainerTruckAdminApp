37bd6d64b107789e94be9e0f9270fdee485c18ae
koplanyidavid <koplanyidavid@gmail.com>
2021-08-24 16:02:53 +0200

    get signed up users done

--- app/src/main/java/com/vicegym/qrtrainertruckadminapp/adapter/TrainingsAdapter.kt
+++ app/src/main/java/com/vicegym/qrtrainertruckadminapp/adapter/TrainingsAdapter.kt
@@ -3,6 +3,8 @@ package com.vicegym.qrtrainertruckadminapp.adapter
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,26 +13,39 @@ import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.vicegym.qrtrainertruckadminapp.TraineesActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.vicegym.qrtrainertruckadminapp.data.TraineeData
 import com.vicegym.qrtrainertruckadminapp.data.TrainingData
 import com.vicegym.qrtrainertruckadminapp.databinding.CardTrainingBinding
+import com.vicegym.qrtrainertruckadminapp.popupwindow.PopupWindowActivity
+
 
 class TrainingsAdapter(private val context: Context) :
     ListAdapter<TrainingData, TrainingsAdapter.TrainingsViewHolder>(ItemCallback) {
 
+    private val db = Firebase.firestore
     private val trainingsList: MutableList<TrainingData> = mutableListOf()
     private var lastPosition = -1
+    private var traineesDataArray: ArrayList<TraineeData> = arrayListOf()
+    private var traineesIdArray: List<String>? = null
 
     class TrainingsViewHolder(binding: CardTrainingBinding) : RecyclerView.ViewHolder(binding.root) {
         val tvTrainingType = binding.tvTrainingType
         val tvTrainer: TextView = binding.tvTrainer
         val tvGymPlace: TextView = binding.tvGymPlace
         val tvDate: TextView = binding.tvGymDate
-        val trainingCard = binding.cardView
+        val trainingCard = binding.cardViewTraining
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        TrainingsViewHolder(CardTrainingBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        TrainingsViewHolder(
+            CardTrainingBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
 
     override fun onBindViewHolder(holder: TrainingsViewHolder, position: Int) {
         val tmpTraining = trainingsList[position]
@@ -38,13 +53,53 @@ class TrainingsAdapter(private val context: Context) :
         holder.tvTrainer.text = tmpTraining.trainer
         holder.tvGymPlace.text = tmpTraining.location
         holder.tvDate.text = tmpTraining.date
-        holder.trainingCard.setOnClickListener { startTraineesActivity() }
+        holder.trainingCard.setOnClickListener { getTraineesId(tmpTraining.id!!) }
         setAnimation(holder.itemView, position)
     }
 
-    private fun startTraineesActivity() {
-        context.startActivity(Intent(context, TraineesActivity::class.java))
+    private fun getTraineesId(id: String) {
+        db.collection("trainings").document(id).get().addOnSuccessListener { document ->
+            if (document != null) {
+                traineesIdArray = document.data?.get("trainees") as List<String>?
+                if (!(traineesIdArray.isNullOrEmpty()))
+                    getUserFromId()
+            } else {
+                Log.d("TraineesList", "No such document")
+            }
+        }
+            .addOnFailureListener { exception ->
+                Log.d("TraineesList", "get failed with ", exception)
+            }
     }
+
+    private fun getUserFromId() {
+        for (id in traineesIdArray!!) {
+            db.collection("users").document(id).get().addOnSuccessListener { document ->
+                if (document != null) {
+                    val traineeData = TraineeData(
+                        document.data?.get("name") as String?,
+                        document.data?.get("mobile") as String?,
+                        document.data?.get("email") as String?,
+                        document.data?.get("rank") as String?
+                    )
+                    if (!(traineesDataArray.contains(traineeData)))
+                        traineesDataArray.add(traineeData)
+
+                    if (traineesDataArray.size == traineesIdArray!!.size)
+                        startTraineesDataPopupWindow()
+                }
+            }
+        }
+    }
+
+    private fun startTraineesDataPopupWindow() {
+        val intent = Intent(context, PopupWindowActivity::class.java)
+        val bundle = Bundle()
+        bundle.putParcelableArrayList("traineeList", traineesDataArray)
+        intent.putExtras(bundle)
+        context.startActivity(intent)
+    }
+
 
     fun addTrainings(training: TrainingData?) {
         training ?: return
--- app/src/main/java/com/vicegym/qrtrainertruckadminapp/data/TraineeData.kt
+++ app/src/main/java/com/vicegym/qrtrainertruckadminapp/data/TraineeData.kt
@@ -1,7 +1,40 @@
 package com.vicegym.qrtrainertruckadminapp.data
 
-class TraineeData (
+import android.os.Parcel
+import android.os.Parcelable
+
+data class TraineeData(
     val name: String? = null,
     val mobile: String? = null,
-    val email: String? = null
-)
+    val email: String? = null,
+    val rank: String? = null
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(mobile)
+        parcel.writeString(email)
+        parcel.writeString(rank)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<TraineeData> {
+        override fun createFromParcel(parcel: Parcel): TraineeData {
+            return TraineeData(parcel)
+        }
+
+        override fun newArray(size: Int): Array<TraineeData?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
